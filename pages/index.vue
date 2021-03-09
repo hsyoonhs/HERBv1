f5a8b74d2f8af6898ffec47ded207f774c8e6f4d
hsyoonhs <safe.exsequi@gmail.com>
2021-03-10 00:53:28 +0900

    메인페이지에 임시링크 추가

--- pages/index.vue
+++ pages/index.vue
@@ -18,6 +18,38 @@
         No other internal dependency
       </card>
     </div>
+    <div class="content">
+      <ul>
+        <li><NuxtLink to="/11">1학년 인반</NuxtLink></li>
+        <li><NuxtLink to="/11">1학년 의반</NuxtLink></li>
+        <li><NuxtLink to="/11">1학년 예반</NuxtLink></li>
+        <li><NuxtLink to="/11">1학년 지반</NuxtLink></li>
+        <li><NuxtLink to="/11">1학년 매반</NuxtLink></li>
+        <li><NuxtLink to="/11">1학년 난반</NuxtLink></li>
+        <li><NuxtLink to="/11">1학년 국반</NuxtLink></li>
+        <li><NuxtLink to="/11">1학년 죽반</NuxtLink></li>
+        <li><NuxtLink to="/21">2학년 인반</NuxtLink></li>
+        <li><NuxtLink to="/22">2학년 의반</NuxtLink></li>
+        <li><NuxtLink to="/23">2학년 예반</NuxtLink></li>
+        <li><NuxtLink to="/24">2학년 지반</NuxtLink></li>
+        <li><NuxtLink to="/25">2학년 매반</NuxtLink></li>
+        <li><NuxtLink to="/26">2학년 난반</NuxtLink></li>
+        <li><NuxtLink to="/27">2학년 국반</NuxtLink></li>
+        <li><NuxtLink to="/28">2학년 죽반</NuxtLink></li>
+        <li><NuxtLink to="/31">3학년 인반</NuxtLink></li>
+        <li><NuxtLink to="/32">3학년 의반</NuxtLink></li>
+        <li><NuxtLink to="/33">3학년 예반</NuxtLink></li>
+        <li><NuxtLink to="/34">3학년 지반</NuxtLink></li>
+        <li><NuxtLink to="/35">3학년 매반</NuxtLink></li>
+        <li><NuxtLink to="/36">3학년 난반</NuxtLink></li>
+        <li><NuxtLink to="/37">3학년 국반</NuxtLink></li>
+        <li><NuxtLink to="/38">3학년 죽반</NuxtLink></li>
+      </ul>
+      <ul>
+        <li><NuxtLink to="/meals">월간 급식표</NuxtLink></li>
+        <li><NuxtLink to="/teachers">교사 시간표</NuxtLink></li>
+      </ul>
+    </div>
   </section>
 </template>
 
