347cf3bf5a3d0f3defa6a253c35023a0d346e67a
hsyoonhs <safe.exsequi@gmail.com>
2021-03-11 00:38:40 +0900

    에러페이지 추가

---
+++ layouts/error.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1 v-if="error.statusCode === 404">페이지를 찾을 수 없습니다.</h1>
+    <h1 v-else>오류가 발생했습니다.</h1>
+    <NuxtLink to="/">홈으로 돌아가기</NuxtLink>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: ['error'],
+    layout: 'error' // you can set a custom layout for the error page
+  }
+</script>
