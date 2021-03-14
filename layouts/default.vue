a5c2f795a4c4f84b58c352689c0eb12d0e7252fd
hsyoonhs <safe.exsequi@gmail.com>
2021-03-15 02:12:12 +0900

    기본 템플릿의 레이아웃 일부 개량

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,24 +1,20 @@
 <template>
   <div>
     <nav
-      class="navbar header has-shadow is-primary"
+      class="navbar header has-shadow is-spaced"
       role="navigation"
       aria-label="main navigation"
     >
-      <div class="navbar-brand">
-        <a class="navbar-item" href="/">
-          <img src="~assets/buefy.png" alt="Buefy" height="28" />
-        </a>
-
-        <div class="navbar-burger">
-          <span />
-          <span />
-          <span />
+      <div class="container is-max-desktop">
+        <div class="navbar-brand">
+          <a class="navbar-item" href="/">
+            <h1 class="title is-4 has-text-weight-bold">HERBv1</h1>
+          </a>
         </div>
       </div>
     </nav>
 
-    <section class="main-content columns">
+    <section class="main-content container is-max-desktop">
       <aside class="column is-2 section">
         <p class="menu-label is-hidden-touch">General</p>
         <ul class="menu-list">
