0c64809166be361be01401ce8331443560552781
hsyoonhs <safe.exsequi@gmail.com>
2021-03-15 02:36:22 +0900

    footer 추가

--- layouts/default.vue
+++ layouts/default.vue
@@ -30,6 +30,16 @@
         <nuxt />
       </div>
     </section>
+
+    <footer class="footer">
+      <div class="content container is-max-desktop has-text-grey-light">
+        <p>
+          개발 및 디자인 ⓒ 2021 Hs Yoon
+          <br>
+          저작권이 명시되지 않은 모든 자료는 청원고등학교에 귀속되어 있습니다.
+        </p>
+      </div>
+    </footer>
   </div>
 </template>
 
