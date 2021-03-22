776d5b1f6742ec087698c12a8d51b98e4f15a945
hsyoonhs <safe.exsequi@gmail.com>
2021-03-22 23:34:37 +0900

    페이지 transition 및 meals 페이지 레이아웃 조정

--- pages/meals.vue
+++ pages/meals.vue
@@ -1,10 +1,12 @@
 <template>
-  <div class="container column is-10">
-    <section class="section">
-      <h2 class="title">🍲 월간 급식</h2>
-      <h3 class="subtitle">이번 달의 급식표 확인하기</h3>
-      <hr />
-      <NuxtLink to="/">홈으로 되돌아가기</NuxtLink>
-    </section>
-  </div>
+  <section class="main-content container is-max-desktop">
+    <div class="container column is-10">
+      <section class="section">
+        <h2 class="title">🍲 월간 급식</h2>
+        <h3 class="subtitle">이번 달의 급식표 확인하기</h3>
+        <hr />
+        <NuxtLink to="/">홈으로 되돌아가기</NuxtLink>
+      </section>
+    </div>
+  </section>
 </template>
