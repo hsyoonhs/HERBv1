68ecf37336c052aaeb4ed694332c9e261957eb97
hsyoonhs <safe.exsequi@gmail.com>
2021-04-11 16:47:53 +0900

    inspire.vue 페이지 삭제, footer 링크를 NuxtLink로 교체

--- layouts/oryu.vue
+++ layouts/oryu.vue
@@ -9,7 +9,7 @@
       <footer class="footer">
         <div class="content container is-max-desktop">
           <h2>HERB<sup>v1</sup></h2>
-          <li><a href="/information">정보</a></li>
+          <li><NuxtLink to="/information">정보</NuxtLink></li>
           <li>개발 및 디자인 ⓒ 2021 Hs Yoon</li>
           <li>
             저작권이 명시되지 않은 모든 자료는 청원고등학교에 귀속되어 있습니다.
