ffacdc864652bd7f82e77a47d759d795ed63dd62
hsyoonhs <safe.exsequi@gmail.com>
2021-04-19 13:55:08 +0900

    반별 페이지 모두 추가

---
+++ pages/27.vue
@@ -0,0 +1,19 @@
+ <template>
+  <div>
+    <NuxtLink to="/">
+      <b-icon
+        icon="chevron-left"
+        size="is-medium"
+        style="vertical-align: middle"
+      ></b-icon>
+      홈으로 돌아가기
+    </NuxtLink>
+  </div>
+</template>
+
+<script>
+export default {
+  layout: 'class',
+}
+</script>
+
