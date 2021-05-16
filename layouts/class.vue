43a0be76b6a0182f431bab703c39891dacc3d3fd
hsyoonhs <80237040+hsyoonhs@users.noreply.github.com>
2021-05-17 01:12:44 +0900

    반별 제목 변경 기능, 급식 식단 출력 기능 추가

--- layouts/class.vue
+++ layouts/class.vue
@@ -35,20 +35,25 @@
         <section class="section">
           <nuxt />
           <div class="columns is-multiline">
-            <div class="column">
-              <div class="card">
-                <header class="card-header">
-                  <p class="card-header-title">카드 헤더</p>
-                </header>
-                <div class="card-content">카드 콘텐츠</div>
+            <div class="column is-full">
+              <div class="card" id="realtime">
+                <div class="card-content"></div>
               </div>
             </div>
             <div class="column">
-              <div class="card">
+              <div class="card" id="timetable">
                 <header class="card-header">
-                  <p class="card-header-title">카드 헤더</p>
+                  <p class="card-header-title">시간표</p>
                 </header>
-                <div class="card-content">카드 콘텐츠</div>
+                <div class="card-content"></div>
+              </div>
+            </div>
+            <div class="column">
+              <div class="card" id="meals">
+                <header class="card-header">
+                  <p class="card-header-title">급식 식단</p>
+                </header>
+                <div class="card-content"></div>
               </div>
             </div>
           </div>
@@ -138,3 +143,99 @@
   -ms-overflow-style: none;
 }
 </style>
+
+<script scoped>
+const classCodeInRaw = window.location.pathname;
+const classCode = classCodeInRaw.replace(/[/]/g, '');
+const classCodeSplitted = classCode.split("");
+const classGrade = classCodeSplitted[0];
+const classNameInNum = classCodeSplitted[1];
+
+if (classNameInNum == 1)
+   className = '인'
+else if (classNameInNum == 2)
+   className = '의'
+else if (classNameInNum == 3)
+   className = '예'
+else if (classNameInNum == 4)
+   className = '지'
+else if (classNameInNum == 5)
+   className = '매'
+else if (classNameInNum == 6)
+   className = '난'
+else if (classNameInNum == 7)
+   className = '국'
+else
+   className = '죽'
+
+document.querySelector('.class-header .title').innerHTML = `${classGrade}학년 ${className}반`;
+
+window.onload = realtimeDate();
+function realtimeDate(){
+  const now = new Date();
+  const nowDate = now.toLocaleDateString();
+  const week = new Array('(일)', '(월)', '(화)', '(수)', '(목)', '(금)', '(토)');
+  const nowWeek = week[now.getDay()];
+  const nowTime = now.toLocaleTimeString();
+  document.querySelector('#realtime .card-content').innerHTML = `${nowDate} ${nowWeek} ${nowTime}`;
+  setTimeout(realtimeDate, 1000);
+}
+
+let date = new Date();
+let year = date.getFullYear();
+let month = new String(date.getMonth() + 1);
+let day = new String(date.getDate());
+
+if(month.length == 1) {
+  month = "0" + month;
+}
+if(day.length == 1) {
+  day = "0" + day;
+}
+
+const todayDate = year + month + day;
+
+const section = document.querySelector('#meals .card-content');
+
+const requestURL = 'https://open.neis.go.kr/hub/mealServiceDietInfo?Type=json&ATPT_OFCDC_SC_CODE=M10&SD_SCHUL_CODE=8000214&MLSV_YMD=' + todayDate;
+
+const request = new XMLHttpRequest();
+request.open('GET', requestURL);
+request.responseType = 'json';
+request.send();
+request.onload = function() {
+    const mealDiet = request.response;
+    showDiet(mealDiet);
+}
+
+function showDiet(jsonObj) {
+  const diet = jsonObj['mealServiceDietInfo'][1]['row'];
+
+  for (var i = 0; i < diet.length; i++) {
+    const myArticle = document.createElement('article');
+    const myH3 = document.createElement('h3');
+    const myList = document.createElement('ul');
+    const myPara = document.createElement('p');
+  
+    const DDISH_NM_TEMP = diet[i].DDISH_NM.replace(/(<([^>]+)>)/ig,", ");
+    const DDISH_NM = DDISH_NM_TEMP.replace(/([\d.])/ig,"");
+    const words = DDISH_NM.split(',');
+
+    myH3.textContent = diet[i].MMEAL_SC_NM;
+
+    const mealList = words;
+    for (let j = 0; j < mealList.length; j++) {
+      const listItem = document.createElement('li');
+      listItem.textContent = mealList[j];
+      myList.appendChild(listItem);
+    }
+
+    myPara.textContent = `칼로리: ${diet[i].CAL_INFO}`;
+
+    myArticle.appendChild(myH3);
+    myArticle.appendChild(myList);
+    myArticle.appendChild(myPara);
+    section.appendChild(myArticle);
+  }
+}
+</script>
